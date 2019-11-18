41000111a32fc7994d480113e993e154251f77ce
Gerardo Alias <gerardo.alias@globant.com>
2019-11-18 19:27:09 -0300

    HL-NFX-0041 - Fix windows scroll bug

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -9,7 +9,7 @@ export const EPISODE_CARDS_AMOUNT = {
   MOBILE_LANDSCAPE: 3,
   MOBILE_PORTRAIT: 2
 };
-export const EPISODE_SLIDER_TRANSLATION_COEF = 97;
+export const EPISODE_SLIDER_TRANSLATION_COEF = 96;
 export const EPISODE_SLIDER_TRANSLATION_EXP = 'translateX(-%svw)';
 export const I18_DEFAULT_TRANSLATION = '####';
 export const I18_KEY_SEPARATOR = '-';
@@ -22,10 +22,10 @@ export const ICON_LIKE = 'fa fa-thumbs-up';
 export const ICON_MY_LIST = 'fa fa-plus';
 export const ICON_PLAY = 'fa fa-play';
 export const ICON_UNLIKE = 'fa fa-thumbs-down';
-export const SCREEN_DESKTOP_MIN_WIDTH = 1200;
-export const SCREEN_MOBILE_LANDSCAPE_MIN_WIDTH = 768;
+export const SCREEN_DESKTOP_MIN_WIDTH = 993;
+export const SCREEN_MOBILE_LANDSCAPE_MIN_WIDTH = 577;
 export const SCREEN_MOBILE_PORTRAIT_MIN_WIDTH = 0;
-export const SCREEN_TABLET_MIN_WIDTH = 992;
+export const SCREEN_TABLET_MIN_WIDTH = 769;
 export const SCROLL_TOPBAR_OFF_SET_CORRECTOR = -50;
 export const TOPBAR_SCROLL_INTERPOLATE_RANGE: Array<number> = [0, 100];
 export const VIDEO_CARDS_AMOUNT = {
@@ -36,5 +36,5 @@ export const VIDEO_CARDS_AMOUNT = {
 };
 export const VIDEO_INFO_TEXT_SEPARATOR = ', ';
 export const VIDEO_OVERLAY_SEPARATOR_CHAR = '•';
-export const VIDEO_SLIDER_TRANSLATION_COEF = 94.4;
+export const VIDEO_SLIDER_TRANSLATION_COEF = 92.4;
 export const VIDEO_SLIDER_TRANSLATION_EXP = 'translateX(-%svw)';
