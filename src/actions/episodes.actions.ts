61b791199451a64c6b8551fd865a320055581662
Gerardo Alias <gerardo.alias@globant.com>
2019-11-27 20:45:39 -0300

    HL-NFX-0060 - Add resources to public folder

--- src/actions/episodes.actions.ts
+++ src/actions/episodes.actions.ts
@@ -2,18 +2,28 @@
 import { Dispatch } from 'redux';
 
 // @Action types
-import  { EPISPDES_SET_EPISODE_DATA } from 'constants/actionTypes';
+import  { EPISPDES_SET_EPISODE_DATA, EPISPDES_SET_EPISODE_DATA_ERR } from 'constants/actionTypes';
 
 // @Constants
 import { Action } from 'constants/types';
 
 // @Utils
 import { mapEpisodes } from 'utils/feedUtils';
+import { localFetch } from 'utils/fetchUtils';
 
-export const getEpisodes = (podcastId: string) => (dispatch: Dispatch): Action => {
-  const episodesData = mapEpisodes(podcastId);
-  return dispatch({
-    type: EPISPDES_SET_EPISODE_DATA,
-    payload: { episodesData }
-  });
+export const getEpisodes = (podcastId: string) => (dispatch: Dispatch): Promise<Action> => {
+  return localFetch('/resources/episodes.json')
+    .then(data => {
+      const episodesData = mapEpisodes(podcastId, data);
+      return dispatch({
+        type: EPISPDES_SET_EPISODE_DATA,
+        payload: { episodesData }
+      });
+    })
+    .catch(error => {
+      return dispatch({
+        type: EPISPDES_SET_EPISODE_DATA_ERR,
+        payload: { error }
+      });
+    });
 };
